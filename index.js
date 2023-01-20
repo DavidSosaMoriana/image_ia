59a01316468cd2e8c388f95bac642825ad691600
DSosa <sosamorianadav@gmail.com>
2023-01-20 19:27:00 +0100

    front end

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => console.log(`server started on port ${port}`))
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/openai', require('./routes/openaiRoutes.js'))
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router()
-const { generateImage } = require('../controllers/openaiControllers')
+const { generateImage } = require('../controllers/openaiControllers');
+const router = express.Router();
 
 router.post('/generateimage', generateImage);
-  
+
 module.exports = router;
